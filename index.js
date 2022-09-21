a6b11a5bfdb44d7cb6d4d256b64b98c324a76acb
White Buffalo <apothocare@gmail.com>
2022-09-21 16:41:29 -0500

    Restructured Question prompts to be in seprate file. Added other class questions

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const inquirer = require('inquirer');
-const {Manager, managerQuestions} = require('./lib/Manager');
+const Manager = require('./lib/Manager');
+const  {managerQuestions, engineerQuestions, internQuestions} = require('./lib/Questions');
+
+
+//Array from team members
+teamArr = [];
+
 
 const getTeam = () => {managerAnswers()}
 const managerAnswers = () => {
     inquirer.prompt(managerQuestions)
-    .then((answers) => {
-        console.log(answers);})
+    .then((data) => { //data is the obj returned from prompt
+        //create manager obj with data that was returned
+        data = new Manager(data.name, data.id, data.email, data.officeNumber)
+        // The manager class now has a new obj with with properties of 'data'
+        //push new manager obj to team member array
+        teamArr.push(data);
+        console.log(teamArr);})
         
     };
 
